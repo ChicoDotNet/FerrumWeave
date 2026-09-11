72b44424e8c18ead7ad885febaa2731ee2f02a1d
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-09-10 20:57:29 -0600

    feat(cil): expose Rust instance projection emitter

--- compiler/cil/src/root.rs
+++ compiler/cil/src/root.rs
@@ -34,6 +34,9 @@ pub use managed_property_export::*;
 mod managed_external_export;
 pub use managed_external_export::*;
 
+mod rust_instance_export;
+pub use rust_instance_export::*;
+
 mod r06;
 pub use r06::*;
 
