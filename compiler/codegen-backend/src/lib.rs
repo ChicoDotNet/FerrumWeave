6da5c8ce7e7cee64dae4b44c85cb5558990c3b64
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-09-13 21:56:42 -0600

    compiler: propagate managed-property export identity

--- compiler/codegen-backend/src/lib.rs
+++ compiler/codegen-backend/src/lib.rs
@@ -13,12 +13,12 @@ use std::{any::Any, fs};
 use ferrumweave_cil::{
     emit_constructible_i32_instance_assembly, emit_disposable_resource_assembly,
     emit_i32_export_with_external_managed_transform, emit_i32_export_with_managed_construction,
-    emit_i32_export_with_named_system_math_method_call,
-    emit_i32_export_with_string_builder_length_property, emit_i32_invalid_operation_export_assembly,
+    emit_i32_export_with_named_system_math_method_call, emit_i32_invalid_operation_export_assembly,
     emit_named_i32_argument_export_assembly, emit_named_i32_arithmetic_export_assembly,
     emit_named_i32_control_flow_export_assembly, emit_named_i32_direct_call_export_assembly,
     emit_named_i32_export_assembly, emit_named_i32_export_with_managed_construction,
-    emit_named_i32_export_with_managed_instance_call, emit_named_i32_method_export_assembly,
+    emit_named_i32_export_with_managed_instance_call,
+    emit_named_i32_export_with_string_builder_length_property, emit_named_i32_method_export_assembly,
     emit_option_reference_export_assembly, emit_option_value_export_assembly,
 };
 use rustc_codegen_ssa::{CodegenResults, CompiledModule, CrateInfo, ModuleKind, TargetConfig, traits::CodegenBackend};
@@ -185,7 +185,14 @@ impl CodegenBackend for FerrumWeaveCodegenBackend {
                         &export_method_name,
                     )
                 }
-                LoweredI32Export::ManagedStringBuilderLength { payload } => emit_i32_export_with_string_builder_length_property(payload),
+                LoweredI32Export::ManagedStringBuilderLength { payload } => {
+                    let export_method_name = managed_export_method_name(tcx)
+                        .unwrap_or_else(|message| panic!("FERRUMWEAVE_MIR_LOWERING_FAILED: {message}"));
+                    emit_named_i32_export_with_string_builder_length_property(
+                        payload,
+                        &export_method_name,
+                    )
+                }
             }
         };
         Box::new(GeneratedArtifact { image, crate_info: CrateInfo::new(tcx, "ferrumweave".to_owned()) })
